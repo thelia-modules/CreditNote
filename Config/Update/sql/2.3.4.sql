efe816436aea09a24e6adbf22b1db4b7d021c882
Florian BERNARD <Cav0n@hotmail.fr>
2020-06-24 10:50:13 +0200

    Fixed 2.3.4 sql update

--- Config/Update/sql/2.3.4.sql
+++ Config/Update/sql/2.3.4.sql
@@ -4,10 +4,15 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 ALTER TABLE credit_note
-DROP COLUMN log_created_at
+DROP COLUMN version
 
 ALTER TABLE credit_note
-DROP COLUMN log_created_by
+DROP COLUMN version_created_at
+
+ALTER TABLE credit_note
+DROP COLUMN version_created_by
+
+DROP TABLE IF EXISTS `credit_note_version`;
 
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
